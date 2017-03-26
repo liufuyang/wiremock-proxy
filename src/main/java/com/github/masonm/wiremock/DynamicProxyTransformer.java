b3bd84dd1b64d63b9c8a6109faaee1e71f66cb14
Mason Malone <mason@masonm.org>
2017-03-25 22:28:29 -0700

    Initial version

---
+++ src/main/java/com/github/masonm/wiremock/DynamicProxyTransformer.java
@@ -0,0 +1,36 @@
+package com.github.masonm.wiremock;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.extension.Parameters;
+import com.github.tomakehurst.wiremock.extension.ResponseDefinitionTransformer;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.http.ResponseDefinition;
+
+public class DynamicProxyTransformer extends ResponseDefinitionTransformer {
+    @Override
+    public ResponseDefinition transform(Request request, ResponseDefinition responseDefinition, FileSource files, Parameters parameters) {
+        URI requestUri;
+        try {
+            requestUri = new URI(request.getAbsoluteUrl());
+        } catch (URISyntaxException e) {
+            return responseDefinition;
+        }
+        String hostHeader = request.getHeader("Host");
+        String proxyUrl = requestUri.getScheme() + "://" + hostHeader;
+        return ResponseDefinitionBuilder.like(responseDefinition).proxiedFrom(proxyUrl).build();
+    }
+
+    @Override
+    public boolean applyGlobally() {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return "dynamic-proxy";
+    }
+}
